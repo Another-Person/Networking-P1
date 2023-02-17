0eec5d5560da3dfd616f6ae1020e73ee52716393
Joey Sachtleben <jsachtleben@carthage.edu>
2023-02-17 15:02:57 -0600

    Added core files

---
+++ client.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <set>
+#include <cstdlib>
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <memory.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "defaults.hpp"
+#include "structure.hpp"
