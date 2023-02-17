b1fcf7cdde85f3fb757ad448d99d94523fc0db5f
Joey Sachtleben <jsachtleben@carthage.edu>
2023-02-17 15:59:02 -0600

    Comments & tweaks

--- client.cpp
+++ client.cpp
@@ -1,9 +1,16 @@
+/* CSC 3600 Spring 2023 Project 1
+ * UDP Blaster
+ * Joey Sachtleben
+ */
+
+// C/C++ Standard Library includes
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <set>
 #include <cstdlib>
 
+// System libraries
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -15,5 +22,32 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+// Local includes
 #include "defaults.hpp"
 #include "structure.hpp"
+
+
+
+int main(int argc, char* argv[])
+{
+    int retval = 0;
+    int udpSocket = -1;
+    int serverPort = PORT_NUMBER;
+    std::string serverName = SERVER_IP;
+    int32_t datagramsToSend = NUMBER_OF_DATAGRAMS;
+
+    char c;
+    while ((c = getopt(argc, argv, "dhs:p:n:y:")) != -1)
+    {
+        switch (c)
+        {
+        case 'h':
+            /* code */
+            break;
+
+        default:
+            break;
+        }
+    }
+
+}
