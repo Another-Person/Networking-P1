4e75bedb5911493a6a0c24ba3af8f90590ceb5c6
Joey Sachtleben <jsachtleben@carthage.edu>
2023-02-23 12:49:39 -0600

    feat: added core server file

---
+++ server.cpp
@@ -0,0 +1,34 @@
+/* CSC 3600 Spring 2023 Project 1
+ * UDP Blaster -- Server
+ * Joey Sachtleben
+ */
+
+// C/C++ Standard Library
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+// System library
+#ifdef	 __linux__
+#include <unistd.h>
+#endif
+
+#include <getopt.h>
+#include <memory.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <assert.h>
+
+// Local includes
+#include "defaults.hpp"
+#include "structure.hpp"
+
+// Constants for errors
+
+int main(int argc, char* argv[])
+{
+
+}
